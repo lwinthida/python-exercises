4aabf7e2655a65a499474b8a2b483628d69b38f8
lwinthida <lwinthida161@gmail.com>
2018-06-06 13:07:03 -0700

    My fifth python exercise

---
+++ ex5.py
@@ -0,0 +1,12 @@
+my_name='Zed A.Show'
+my_age=35#not a line
+my_height=74 # inches
+my_weight=180 #lbs
+my_eyes='Blue'
+my_teeth='White'
+my_hair='Brown'
+print "Let's talk about %s." %my_name
+print "He's %d inches tall." %my_height
+print "He's got %s eyes and  %s hair." %(my_eyes,my_hair)
+#this line is tricky try to get it exactly right 
+print "If I add %d , %d, and % d I get %d."%(my_age,my_height,my_weight,my_age+my_height-my_weight)
