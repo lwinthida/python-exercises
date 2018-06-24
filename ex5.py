91e783fb7c249d57575c65e264ac8debf22618a9
lwinthida <lwinthida161@gmail.com>
2018-06-23 23:49:47 -0700

    Add more variables and printing

--- ex5.py
+++ ex5.py
@@ -5,8 +5,13 @@ my_weight=180 #lbs
 my_eyes='Blue'
 my_teeth='White'
 my_hair='Brown'
-print "Let's talk about %s." %my_name
-print "He's %d inches tall." %my_height
-print "He's got %s eyes and  %s hair." %(my_eyes,my_hair)
-#this line is tricky try to get it exactly right 
-print "If I add %d , %d, and % d I get %d."%(my_age,my_height,my_weight,my_age+my_height-my_weight)
+print(f"Let's talk about { my_name}.")
+print(f"He's {my_height} inches tall.")
+print(f"He's {my_weight} pounds heavy.")
+print("Actually that's not too heavy.")
+print(f"He's got {my_eyes} eyes and  {my_hair} hair.")
+print(f"His teeth are usually {my_teeth} depending on the person.")
+
+#this line is tricky try to get it exactly right
+total=my_age+my_height+my_weight
+print(f"If I add {my_age}, {my_height}, and {my_weight} I get {total}.")
