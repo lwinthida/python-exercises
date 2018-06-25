4a29e8f38cd598f179fd60150a42ce170e931269
lwinthida <lwinthida161@gmail.com>
2018-06-25 09:15:41 -0700

    User prompting with keyboard

--- ex11.py
+++ ex11.py
@@ -1,7 +1,7 @@
-print "How old are you?",
-age =raw_input()
-print "How tall are you?",
-height=raw_input()
-print "How much do u weight?",
-weight =raw_input()
-print "So,you're %r old,%r tall and %r heavy." %(age,height,weight)
+print("How old are you?", end=' ')
+age = input()
+print("How tall are you?", end=' ')
+height = input()
+print("How much do u weight?", end=' ')
+weight = input()
+print(f"So,you're {age} old,{height} tall and {weight} heavy.")
