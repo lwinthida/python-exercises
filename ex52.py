9c2c62ea786794476cb96f531d1cc93682568e4b
lwinthida <lwinthida161@gmail.com>
2018-06-26 13:43:49 -0700

    Add python ex

---
+++ ex52.py
@@ -0,0 +1,16 @@
+from flask import Flask
+from flask import render_template
+from flask import request
+app=Flask(__name__)
+@app.route("/hello",methods=['POST','GET'])
+def index():
+    greeting="Hello World"
+    if request.method=="POST":
+        name=request.form['name']
+        greet=request.form['greet']
+        greeting=f"{greet},{name}"
+        return render_template("index.html",greeting=greet)
+    else:
+        return render_template("hello_form.html")
+if __name__=="__main__":
+    app.run()
