ab205f93c272fccff9f5df53ba9e8cda2ea1b109
lwinthida <lwinthida161@gmail.com>
2018-06-27 09:18:29 -0700

    Change dog

--- ex101.py
+++ ex101.py
@@ -1,14 +1,14 @@
-tabby_cat = "\t I'm tabbed in."
-persian_cat = "I'm split \n on a line."
-backslash_cat = "I'm \\ a \\ cat."
-fat_cat = """
+tabby_dog = "\t I'm tabbed in."
+persian_dog = "I'm split \n on a line."
+backslash_dog = "I'm \\ a \\  dog."
+fat_dog = """
 I'll do a list:
-    \t* Cat found
+    \t* Dog found
     \t* Fishes
     \t* Catnip \n \t* Grass
     """
     
-print(tabby_cat)
-print(persian_cat)
-print(backslash_cat)
-print(fat_cat)
+print(tabby_dog)
+print(persian_dog)
+print(backslash_dog)
+print(fat_dog)
